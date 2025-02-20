a561c3a8e01cd7add16203d876f2229683bf3632
KaushalKishorMishra <077bcsit016.kaushal@scst.edu.np>
2025-02-20 20:14:57 +0545

    feat(yazi): added yazi
    -> all the configuration for yazi terminal file manager is added to
    dotfiles to track it with git and store init

---
+++ .config/yazi/init.lua
@@ -0,0 +1,20 @@
+-- git config
+require("git"):setup()
+
+-- design config
+require("full-border"):setup()
+
+-- display information associated with the file on the right side of the file list row.
+function Linemode:size_and_mtime()
+	local time = math.floor(self._file.cha.mtime or 0)
+	if time == 0 then
+		time = ""
+	elseif os.date("%Y", time) == os.date("%Y") then
+		time = os.date("%b %d %H:%M", time)
+	else
+		time = os.date("%b %d  %Y", time)
+	end
+
+	local size = self._file:size()
+	return string.format("%s %s", size and ya.readable_size(size) or "-", time)
+end
